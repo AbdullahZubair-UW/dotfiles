0fc2fffe206061ce3083760587b886ef46299367
Abdullah Zubair <a6zubair@uwaterloo.ca>
2022-08-09 13:11:32 -0400

    updated files

--- .config/nvim/lua/user/vimtex.lua
+++ .config/nvim/lua/user/vimtex.lua
@@ -2,6 +2,6 @@ vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_view_general_viewer = 'zathura'
 vim.g.vimtex_compiler_method = 'latexmk'
 -- vim.g.vimtex_quickfix_enabled = 0
-vim.g.vimtex_quickfix_method='pplatex'
+-- vim.g.vimtex_quickfix_method='pplatex'
 vim.g.vimtex_quickfix_autoclose_after_keystrokes=1
 vim.g.vimtex_quickfix_open_on_warning=0
