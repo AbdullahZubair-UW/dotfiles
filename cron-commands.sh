23e4ade8faef73fa2604f4afaddd70a5f1bf944d
Abdullah Zubair <abdzub@hotmail.com>
2022-11-02 09:23:38 -0400

    new changes

--- cron-commands.sh
+++ cron-commands.sh
@@ -6,9 +6,6 @@
 # Updates all git repos (update command is in git alias's)
 git update
 
-# Cleans all latex junk files (.log,.aux,...) in current semester at UW
-find ~/uwaterloo/UW-4A -maxdepth 3 -type d \( ! -name . \) -exec bash -c "cd '{}' && latexmk -c" \;
-
 # Updates uwaterloo dir at Docs
 rsync -avzu --delete -h --exclude='.git/' ~/uwaterloo/ ~/Documents/uwaterloo-backup/
 
