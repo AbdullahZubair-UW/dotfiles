77f49b75f5e2ce2c09c948080d2435a313922451
Abdullah Zubair <abdzub@hotmail.com>
2022-11-03 14:36:45 -0400

    new changes

---
+++ scripts/uwaterloo-update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Updates waterloo repo
+git -C ~/uwaterloo/ ss
+
+# Cleans all latex junk files (.log,.aux,...) in current semester at UW
+find ~/uwaterloo/UW-4A -maxdepth 3 -type d \( ! -name . \) -exec bash -c "cd '{}' && latexmk -c" \;
+
